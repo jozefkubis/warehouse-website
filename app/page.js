ccd497fb17ff413cd33db33a2aedba34cf9b19e1
jozef kubis <kubis.jozef@outlook.com>
2024-12-28 18:49:47 +0100

    Vytvorene prve routes Products a Account

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import React from "react"
 function Page() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="font-bold text-2xl">Welcome to the Main Next Page</h1>
+      <h1 className="font-bold text-2xl">
+        Welcome to the Main Next Warehouse Page
+      </h1>
     </div>
   )
 }
