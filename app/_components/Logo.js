6693dbb7813d22364b455d641dfce2e51efd4745
jozef kubis <kubis.jozef@outlook.com>
2024-12-28 21:35:56 +0100

    Kozmeticka uprava

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -6,7 +6,7 @@ function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
       {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
-      <Image src={logo} height="60" width="60" alt="The Wild Oasis logo" />
+      <Image src={logo} height="60" width="60" alt="Warehouse logo" />
       <span className="text-xl font-semibold text-primary-100">Warehouse</span>
     </Link>
   )
