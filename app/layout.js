eb88ce538334e71ae887e9e70eb602ba3f2c3a35
jozef kubis <kubis.jozef@outlook.com>
2024-12-29 19:54:13 +0100

    Stylovanie body v layout a pridany subor instal.txt pre insalovane npm...

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative">
         <header>
           <Logo />
           <Navigation />
