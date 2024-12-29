e2cbaded5af7f68df5a9428bd39e41141dce8fb4
jozef kubis <kubis.jozef@outlook.com>
2024-12-29 20:31:52 +0100

    Pridany font

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,15 @@ import "@/app/_styles/globals.css"
 import Navigation from "@/app/_components/Navigation"
 import Logo from "@/app/_components/Logo"
 
+import { Noto_Sans } from "next/font/google"
+
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  display: "swap",
+})
+
+console.log(notoSans)
+
 export const metadata = {
   title: {
     template: "%s / Warehouse",
@@ -14,7 +23,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative">
+      <body
+        className={`${notoSans.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
         <header>
           <Logo />
           <Navigation />
