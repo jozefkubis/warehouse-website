2185857e46867ffc67d5271450aea9ceb5e5d7e8
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-01-07 20:18:54 +0100

    Refactor filtering logic in DisplayedProducts and Filter components; update search term handling in page.js

--- app/_components/Count.js
+++ app/_components/Count.js
@@ -1,34 +1,41 @@
-"use client"
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Count() {
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(1);
 
   function handleClickInc() {
-    setCount(count + 1)
+    setCount(count + 1);
   }
 
   function handleClickDec() {
     if (count > 0) {
-      setCount(count - 1)
+      setCount(count - 1);
     }
   }
 
   return (
     <div className="flex gap-4 items-center">
-      <div>
-        <button onClick={handleClickDec}>-</button>
-      </div>
-      <div>{count}</div>
-      <div>
-        <button onClick={handleClickInc}>+</button>
-      </div>
-      <div>
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          add to card
-        </button>
-      </div>
+      <button
+        onClick={handleClickDec}
+        className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-200"
+      >
+        -
+      </button>
+      <span className="text-lg">{count}</span>
+      <button
+        onClick={handleClickInc}
+        className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition duration-200"
+      >
+        +
+      </button>
+      <button
+        className="bg-accent-500 text-white px-6 py-2 ml-4 rounded-lg font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+        disabled={count === 0}
+      >
+        Add to Card
+      </button>
     </div>
-  )
+  );
 }
