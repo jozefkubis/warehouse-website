1cf1b15b73afe957c6e38fb130d766dad70b5d6b
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-01-10 14:06:56 +0100

    Aplikovany middleware, SignIn Button a SignInPage

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -9,36 +9,36 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
-  // callbacks: {
-  //     authorized({ auth, request }) {
-  //         return !!auth?.user
-  //     },
-  //     async signIn({ user, account, profile }) {
-  //         try {
-  //             const existingGuest = await getGuest(user.email)
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user
+    },
+    //     async signIn({ user, account, profile }) {
+    //         try {
+    //             const existingGuest = await getGuest(user.email)
 
-  //             if (!existingGuest) await createGuest({ email: user.email, fullName: user.name })
+    //             if (!existingGuest) await createGuest({ email: user.email, fullName: user.name })
 
-  //             return true
-  //         } catch {
-  //             return false
-  //         }
-  //     },
-  //     async session({ session, user }) {
-  //         const guest = await getGuest(session.user.email)
-  //         session.user.guestId = guest.id
+    //             return true
+    //         } catch {
+    //             return false
+    //         }
+    //     },
+    //     async session({ session, user }) {
+    //         const guest = await getGuest(session.user.email)
+    //         session.user.guestId = guest.id
 
-  //         return session
-  //     }
-  // },
-  //   pages: {
-  //     signIn: "/login",
-  //   },
+    //         return session
+    //     }
+  },
+  pages: {
+    signIn: "/login",
+  },
 }
 
 export const {
   auth,
-  //   signIn,
-  //   signOut,
+  signIn,
+  signOut,
   handlers: { GET, POST },
 } = NextAuth(authConfig)
