eb94d2e84d98bdc468cd5454ee71c3ffee078e05
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-01-13 21:49:18 +0100

    Pridany border do Carousel

--- app/_components/CarouselCard.js
+++ app/_components/CarouselCard.js
@@ -5,7 +5,7 @@ export default function CarouselCard({ product }) {
     return (
         <div className="flex flex-col" >
             <div className="flex relative h-56 w-auto flex-col">
-                <Image src={product.image} alt={`Product ${product.name}`} fill className="rounded-t-md object-cover" />
+                <Image src={product.image} alt={`Product ${product.name}`} fill className="rounded-t-md object-cover border-4 border-primary-500" />
             </div>
             <div className="flex flex-col items-center justify-between h-20 bg-primary-500 text-primary-900 font-semibold  p-2 rounded-b-md">
                 <p className="text-center text-[0.9rem]">{(product.name).split(" ").slice(0, 2).join(" ")}</p>
