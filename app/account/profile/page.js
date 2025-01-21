8c01daae4e9736811f4fda1c86d877cd0b9207fa
jozef kubis <kubis.jozef@outlook.com>
2025-01-21 19:22:21 +0100

    Pridany session a vymazany selectcountry v profile: page

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,13 +1,14 @@
-import SelectCountry from "@/app/_components/SelectCountry"
 import UpdateProfileForm from "@/app/_components/UpdateProfileForm"
+import { auth } from "@/app/_lib/auth"
+import { getCustomer } from "@/app/_lib/data-service"
 
 export const metadata = {
   title: "Update profile",
 }
 
-export default function Page() {
-  const countryFlag = "pt.jpg"
-  const nationality = "portugal"
+export default async function Page() {
+  // const session = await auth()
+  // const customer = getCustomer(session.user.email)
 
   return (
     <div>
@@ -17,17 +18,10 @@ export default function Page() {
 
       <p className="text-lg mb-8 text-primary-200">
         Providing the following information will make your ordering process
-        faster and smoother.
+        faster and smoother.h
       </p>
 
-      <UpdateProfileForm>
-        <SelectCountry
-          name="nationality"
-          id="nationality"
-          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-          defaultCountry={nationality}
-        />
-      </UpdateProfileForm>
+      <UpdateProfileForm />
     </div>
   )
 }
